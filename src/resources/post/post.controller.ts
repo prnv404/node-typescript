af61d416008712aacb10d99593a33500591ffe64
pranav <pranavofficial404@gmail.com>
2022-11-12 12:19:49 +0530

    post-controller created

--- src/resources/post/post.controller.ts
+++ src/resources/post/post.controller.ts
@@ -1,2 +1,17 @@
-import { Request, Response, NextFunction } from 'express'
+import { Router, Request, Response, NextFunction } from 'express'
+import Controller from '@/utils/interfaces/controller.interface'
+import HttpException from '@/utils/exception/http.exception'
+import validationMiddleware from '@/middleware/validation.middleware'
+import validate from './post.validation'
+import PostService from './post.service'
 
+class PostController implements Controller {
+    public path = '/posts'
+    public router = Router()
+    constructor() {
+        this.initialiseRoutes()
+    }
+    private initialiseRoutes(): void{
+        
+    }
+}
